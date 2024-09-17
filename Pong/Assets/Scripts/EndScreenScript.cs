90b3bb5f9158b379e0bd959d9fcb4342d0688681
Simon-Nicolas Lojnik <loj210680@spengergasse.at>
2024-09-17 10:21:38 +0200

    End Screen

---
+++ Pong/Assets/Scripts/EndScreenScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndScreenScript : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text TextWinner;
+    [SerializeField]
+    private TMP_Text TextEndScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextWinner.text = (GlobalData.LastWin == 1) ? "Player Left Wins" : "Player Right Wins";
+        TextEndScore.text = (GlobalData.LastWin == 1) ? $"{GlobalData.RightScore} : {GlobalData.LeftScore}": $"{GlobalData.LeftScore} : {GlobalData.RightScore}";
+    }
+}
